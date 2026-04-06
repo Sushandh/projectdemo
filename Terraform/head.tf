582282e83fb740aae5661765fefdfac12d78870f
Sushandh <ktsushandh978@gmail.com>
2026-04-06 08:37:11 +0100

    removed public access to fix s3 error

--- Terraform/head.tf
+++ Terraform/head.tf
@@ -15,28 +15,7 @@ resource "aws_s3_bucket_website_configuration" "config" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "public" {
-  bucket = aws_s3_bucket.website.id
 
-  block_public_acls   = false
-  block_public_policy = false
-  restrict_public_buckets = false
-  ignore_public_acls  = false
-}
-
-resource "aws_s3_bucket_policy" "policy" {
-  bucket = aws_s3_bucket.website.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = "*",
-      Action = ["s3:GetObject"],
-      Resource = "${aws_s3_bucket.website.arn}/*"
-    }]
-  })
-}
 
 # ---------------- METRICS ----------------
 resource "aws_s3_bucket_metric" "metrics" {

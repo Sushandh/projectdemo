aa65faef8874642711648f81f7703d158f50d14d
Sushandh <ktsushandh978@gmail.com>
2026-04-06 08:13:02 +0100

    updated email for SNS alerts

--- Terraform/head.tf
+++ Terraform/head.tf
@@ -52,7 +52,7 @@ resource "aws_sns_topic" "alerts" {
 resource "aws_sns_topic_subscription" "email" {
   topic_arn = aws_sns_topic.alerts.arn
   protocol  = "email"
-  endpoint  = "your-email@example.com" # 🔥 change this
+  endpoint  = "ktsushandh978@gmail.com" # 🔥 change this
 }
 
 # ---------------- CLOUDWATCH ALARM ----------------
